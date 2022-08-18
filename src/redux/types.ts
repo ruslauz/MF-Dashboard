8c7863e392279d5dc28bc172587da5cdfc9a3d00
Sergey Fomin <sergey.fomin@semrush.com>
2022-08-19 00:50:52 +0300

    redeploy

--- src/redux/types.ts
+++ src/redux/types.ts
@@ -1,10 +1,13 @@
 import store from "./store";
 
 export type Store = typeof store;
+
 export type RootState = ReturnType<typeof store.getState> & {
   [key: string]: Record<string, any>;
 };
+
 export type AppDispatch = typeof store.dispatch;
+
 export enum WidgetsType {
   Old = "old",
   UIKit = "uiKit",
